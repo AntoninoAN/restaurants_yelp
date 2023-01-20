f5e64f6661cf2d207c874ba12e76c37b14a7c9c1
illeagle2 <mdilleagle@gmail.com>
2023-01-20 13:09:09 -0500

    testing commit from matthew

--- app/src/main/java/com/example/restaurantsyelpcompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/restaurantsyelpcompose/ui/screens/HomeScreen.kt
@@ -1,8 +1,35 @@
 package com.example.restaurantsyelpcompose.ui.screens
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.restaurantsyelpcompose.MainApp
 
 @Composable
 fun HomeScreen() {
 
+}
+
+@Composable
+fun HomeScreenContentLoading(isLoading: Boolean) {
+    //is loading display progress bar
+    //not loading hide progress bar
+    AnimatedVisibility(visible = isLoading) {
+        CircularProgressIndicator(
+            modifier = Modifier.padding(4.dp),
+                    strokeWidth = 10.dp
+        )//matthew was here
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreenLoading(){
+    MainApp {
+        HomeScreenContentLoading(isLoading = true)
+    }
 }
