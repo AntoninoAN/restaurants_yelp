7b8b90a18ee479bb8aea8b1144fd828bb7784e61
AntoninoAN <andonino@gmail.com>
2023-01-20 10:23:19 -0500

    first commit

---
+++ app/src/main/java/com/example/restaurantsyelpcompose/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.restaurantsyelpcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.restaurantsyelpcompose.ui.screens.HomeScreen
+import com.example.restaurantsyelpcompose.ui.theme.RestaurantsYelpComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainApp {
+                val navController = rememberNavController()
+
+                NavHost(navController = navController,
+                    startDestination = "home") {
+                    composable(route = "home") {
+                        HomeScreen()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainApp(content: @Composable ()->Unit) {
+    RestaurantsYelpComposeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MainApp {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/example/restaurantsyelpcompose/ui/screens/HomeScreen.kt
@@ -0,0 +1,8 @@
+package com.example.restaurantsyelpcompose.ui.screens
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+
+}
