ec3819b8469ef6682e7fdf8a41905b403bc9b27f
gguij004 <51983923+gguij004@users.noreply.github.com>
2023-01-23 10:48:30 -0500

    initial UI work

--- app/src/main/java/com/example/restaurantsyelpcompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/restaurantsyelpcompose/ui/screens/HomeScreen.kt
@@ -1,8 +1,23 @@
 package com.example.restaurantsyelpcompose.ui.screens
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun HomeScreen() {
 
+    val tempListOfRestaurants = listOf("Chipotle", "Taco Bell", "MacDonal")
+    MessageList(tempListOfRestaurants)
+
+}
+
+@Composable
+fun MessageList(messages: List<String>) {
+    LazyColumn {
+        items(messages) { message ->
+            Text(message)
+        }
+    }
 }
