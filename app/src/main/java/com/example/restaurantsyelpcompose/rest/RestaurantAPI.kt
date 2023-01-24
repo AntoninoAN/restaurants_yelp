71a24f94fe1382c959fce5d948c93470b4eb119e
consultant <nsenelier@gmail.com>
2023-01-24 09:23:37 -0500

    Service API

---
+++ app/src/main/java/com/example/restaurantsyelpcompose/rest/RestaurantAPI.kt
@@ -0,0 +1,17 @@
+package com.example.restaurantsyelpcompose.rest
+
+import com.example.restaurantsyelpcompose.model.RestaurantResponse
+import com.example.restaurantsyelpcompose.utils.ENDPOINT
+import com.example.restaurantsyelpcompose.utils.PARAM_UID
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+interface RestaurantAPI {
+    @GET(ENDPOINT)
+    suspend fun getRestaurantDetails(
+        @Path(PARAM_UID) uid: String
+    ) : Response<RestaurantResponse>
+}
