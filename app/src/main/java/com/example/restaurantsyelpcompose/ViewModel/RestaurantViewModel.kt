f1406638d45621f2e21945fed5bc051b5e3bdf2c
AntoninoAN <andonino@gmail.com>
2023-01-25 10:36:24 -0500

    request restaurant data using latitude and longitude

--- app/src/main/java/com/example/restaurantsyelpcompose/ViewModel/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantsyelpcompose/ViewModel/RestaurantViewModel.kt
@@ -3,9 +3,12 @@ package com.example.restaurantsyelpcompose.ViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.restaurantsyelpcompose.ViewModel.Utils.UIState
+import com.example.restaurantsyelpcompose.rest.Repository
+import kotlinx.coroutines.launch
 
-class RestaurantViewModel: ViewModel() {
+class RestaurantViewModel(private val repository: Repository): ViewModel() {
 
     /**this is our RestaurantViewModel
     //add varialbles
@@ -18,4 +21,13 @@ class RestaurantViewModel: ViewModel() {
     private val _data = MutableLiveData<UIState>()
     val data: LiveData<UIState>
     get() = _data
+
+    fun getRestaurantList(latitude: Double, longitude: Double){
+        repository.getRestaurantList(latitude, longitude)
+        viewModelScope.launch {
+            repository.restaurant.collect {
+                _data.value = it
+            }
+        }
+    }
 }
