2dc9d8ce0c814be8de72606272e9685831091093
illeagle2 <mdilleagle@gmail.com>
2023-01-25 10:32:58 -0500

    implemented methods for restaurant view model

--- app/src/main/java/com/example/restaurantsyelpcompose/ViewModel/RestaurantViewModel.kt
+++ app/src/main/java/com/example/restaurantsyelpcompose/ViewModel/RestaurantViewModel.kt
@@ -1,21 +1,53 @@
 package com.example.restaurantsyelpcompose.ViewModel
 
+import android.location.Location
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.restaurantsyelpcompose.ViewModel.Utils.UIState
+import com.example.restaurantsyelpcompose.rest.Repository
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
-class RestaurantViewModel: ViewModel() {
 
-    /**this is our RestaurantViewModel
-    //add varialbles
-    //add livedata mutable live data
-    //add livedata of ui state
-    //methods getrestaurantlist & getrestaurantdetials
-    // that receives UID string
-    */
+class RestaurantViewModel(private val restaurantsRepository: Repository): ViewModel() {
 
     private val _data = MutableLiveData<UIState>()
     val data: LiveData<UIState>
     get() = _data
+
+    //default state as empty
+    var restaurantUIState: UIState = UIState.Empty
+
+    //Functions to call for network request
+    fun getRestaurantList (location: Location) {
+
+        viewModelScope.launch {
+             restaurantUIState = UIState.Loading
+             restaurantUIState = try {
+                 UIState.Success(restaurantsRepository.getRestaurantList(location))
+             } catch (e: IOException) {
+                 UIState.Failure("$e")
+             } catch (e: HttpException) {
+                 UIState.Failure("$e")
+             }
+        }
+    }
+
+    fun getRestaurantDetails (uid: String) {
+        viewModelScope.launch {
+            restaurantUIState = UIState.Loading
+            restaurantUIState = try {
+                UIState.Success(restaurantsRepository.getRestaurantDetails(uid))
+            } catch (e: IOException) {
+                UIState.Failure("$e")
+            } catch (e: HttpException) {
+                UIState.Failure("$e")
+            }
+        }
+    }
 }
